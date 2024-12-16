dc600196d3dc3c545862ea1660d637760bf57d25
Shahredza <izrin96@gmail.com>
2024-12-16 14:39:13 +0800

    fix: use `next/link`

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,15 @@
 import { Container } from "../ui";
 import UserSearch from "../user-search";
+import Link from "next/link";
 
 export default async function Navbar() {
   return (
     <nav className="sticky left-0 right-0 top-0 h-14 z-30 bg-bg/80">
       <Container className="flex justify-center">
         <div className="grow gap-4 flex items-center h-14">
-          <a href="/">
+          <Link href="/">
             <span className="font-semibold text-xl">Lunar</span>
-          </a>
+          </Link>
         </div>
         <div className="flex items-center gap-2">
           <UserSearch />
