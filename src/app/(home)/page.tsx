1b17c2ec59a1ee4db58a19aa3fd12e3285c88b3f
Shahredza <izrin96@gmail.com>
2024-12-30 20:25:11 +0800

    chore: force-static on `/` route

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import IndexView from "@/components/index/index-view";
 import { getArtistsWithMembers } from "../data-fetching";
 import { fetchObjektsIndex } from "@/lib/server/objekts/objekt-index";
 
+export const revalidate = 0
+
 export default async function Home() {
   const [objekts, artists] = await Promise.all([
     fetchObjektsIndex(),
