6176292a60bb23c3a92f0cd2aea6712cb5081aeb
Shahredza <izrin96@gmail.com>
2024-12-30 20:12:18 +0800

    chore: remove ISR revalidate

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,8 +2,6 @@ import IndexView from "@/components/index/index-view";
 import { getArtistsWithMembers } from "../data-fetching";
 import { fetchObjektsIndex } from "@/lib/server/objekts/objekt-index";
 
-export const revalidate = 60
-
 export default async function Home() {
   const [objekts, artists] = await Promise.all([
     fetchObjektsIndex(),
